4ff4a4a4da88f7f1c1ef06190f76bf750706d930
Chris Franklin <cf73021@gmail.com>
2018-08-21 20:22:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   attr_accessor :balance, :status
   attr_reader :sender
 
-  def initialize(name, balance=1000, status="open")
-    @name = name
+  def initialize(sender, balance=1000, status="open")
+    @sender = name
   end
 end
