b3565a2b9bb65d226802c9b8ccc20e6cd68574d4
Chris Franklin <cf73021@gmail.com>
2018-08-21 20:33:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,13 +2,14 @@ class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
 
-@count = 0
+
 
   def initialize(sender, receiver, status="pending", amount)
     @sender = sender
     @receiver = receiver
     @status = status
     @amount = amount
+    @count = 0
   end
 
   def valid?
