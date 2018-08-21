8db21dfd5dc8a36e9635bdd246f47e86846d724c
Chris Franklin <cf73021@gmail.com>
2018-08-21 20:38:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       sender.withdraw(amount)
       receiver.deposit(amount)
       @status = "complete"
-    elsif sender.valid? == false
+    else
       return "Transaction rejected. Please check your account balance."
     end
   end
