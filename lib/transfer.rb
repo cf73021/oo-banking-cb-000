ac49fd35552cbc4e08544a807b9afaa1c305b8d5
Chris Franklin <cf73021@gmail.com>
2018-08-21 20:38:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @count == 0
+    if sender.valid? && @count == 0
       @count = 1
       sender.withdraw(amount)
       receiver.deposit(amount)
