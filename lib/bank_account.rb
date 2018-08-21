eb7c439a9233f77b34db45e84eaa1daa790242e2
Chris Franklin <cf73021@gmail.com>
2018-08-21 19:52:01 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
   attr_accessor :name, :balance, :status
 
-  def initialize(name, balance=1000)
+  def initialize(name, balance=1000, status="open")
     @name = name
     @balance = balance
   end
