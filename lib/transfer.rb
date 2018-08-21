70408c686a6f25899c37ff252ffbbf2267e08730
Chris Franklin <cf73021@gmail.com>
2018-08-21 20:33:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       @status = "complete"
       @count = 1
     else
-      return false
+      return "Transaction rejected. Please check your account balance."
     end
   end
 end
